fbfa1aa4b4dfcfcb59b5f74dc1e0bd0631fdf86b
Valentin Schaar <valentin.schaar@web.de>
2023-06-02 14:38:14 +0200

    Added signs render.

--- src/gui/field.py
+++ src/gui/field.py
@@ -31,10 +31,10 @@ class Field:
 
         self.field = [
             [
-                [1, 0, 1],
+                [1, 0, -1],
                 [0, 0, 1],
                 [1, 0, 0]
-            ]
+            ],
             [0, 0, 0]*3,
             [0, 0, 0]*3
         ]
@@ -46,7 +46,7 @@ class Field:
 
         pygame.draw.rect(grid, "orange", pygame.Rect(320, 40, 640, 640), 15)
             # Border
-
+        
         pygame.draw.line(grid, "orange", (340, 260), (940, 260), 8) # Top
         pygame.draw.line(grid, "orange", (340, 460), (940, 460), 8) # Bot
         pygame.draw.line(grid, "orange", (540, 60), (540, 660), 8) # Left
@@ -54,12 +54,19 @@ class Field:
 
         # Render crosses and circles
 
+        signs = pygame.surface.Surface((1280, 720)).convert_alpha()
+        signs.fill((0, 0, 0, 0))
+
         for x in range(2):
             for y in range(2):
-
-                if self.field[layer][y][x]:
-                    ...
-
-
+                
+                if self.field[layer][y][x] == const.CROSS:
+                    
+                    pygame.draw.line(
+                        signs, "green", (340+x*100, 60+y*100),
+                        (340+x*100, 60+y*100))
+                
+                elif self.field[layer][y][x] == const.CIRCLE:
+                    signs += [(self.image_circle, (340+x*100, 60+y*100))]
 
         return [(grid, (0, 0))]
